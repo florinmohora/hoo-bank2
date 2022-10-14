05878fa3d7659e8cde10e93bdf3b03f31d3af7ad
Mihnea Georgescu <mihtitan@gmail.com>
2022-10-14 19:11:45 +0300

    ascascas

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,20 +1,106 @@
 import styles from "../style";
 import Button from "./Button";
+import GetStarted from "./GetStarted";
 
 const CTA = () => (
-  <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
-    <div className="flex-1 flex flex-col">
-      <h2 className={styles.heading2}>Let’s try our service now!</h2>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        Everything you need to accept card payments and grow your business
-        anywhere on the planet.
-      </p>
-    </div>
-
-    <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
-      <Button />
-    </div>
-  </section>
+<section className="">
+  <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+      <div className="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
+         
+          <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-white bg-black-gradient-2 rounded-lg border border-gray-100 shadow border-gray-600 xl:p-8 bg-gray-800 text-white">
+          <h3 class="mb-4 font-poppins font-semibold text-[33px] leading-[32px] text-white">Herman Jensen</h3>
+              <p className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] text-white sm:text-lg text-white">Best option for personal use & for your next project.</p>
+              <div className="flex justify-center items-baseline my-8">
+                  <span className="mr-2 text-5xl font-extrabold">$29</span>
+                  <span className="text-white text-white">/month</span>
+              </div>
+     
+              <ul role="list" className="mb-8 space-y-4 text-left">
+                  <li className="flex items-center space-x-3">
+                      <svg className="flex-shrink-0 w-5 h-5 text-cyan-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                      <span className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">Individual configuration</span>
+                  </li>
+                  <li className="flex items-center space-x-3">
+                      <svg className="flex-shrink-0 w-5 h-5 text-cyan-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                      <span className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">No setup, or hidden fees</span>
+                  </li>
+                  <li className="flex items-center space-x-3">
+                      <svg className="flex-shrink-0 w-5 h-5 text-cyan-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                      <span className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">Team size: <span className="font-semibold">1 developer</span></span>
+                  </li>
+                  <li className="flex items-center space-x-3">
+                      <svg className="flex-shrink-0 w-5 h-5 text-cyan-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                      <span className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">Premium support: <span className="font-semibold">6 months</span></span>
+                  </li>
+                  <li className="flex items-center space-x-3">
+                      <svg className="flex-shrink-0 w-5 h-5 text-cyan-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                      <span className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">Free updates: <span className="font-semibold">6 months</span></span>
+                  </li>
+              </ul>
+<Button />          </div>
+          <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-white bg-black-gradient-2 rounded-lg border border-gray-100 shadow border-gray-600 xl:p-8 bg-gray-800 text-white">
+          <h3 class="mb-4 font-poppins font-semibold text-[33px] leading-[32px] text-white">Herman Jensen</h3>
+              <p className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] text-white sm:text-lg text-white">Relevant for multiple users, extended & premium support.</p>
+              <div className="flex justify-center items-baseline my-8">
+                  <span className="mr-2 text-5xl font-extrabold">$99</span>
+                  <span className="text-white text-white" text-white>/month</span>
+              </div>
+              <ul role="list" className="mb-8 space-y-4 text-left">
+                  <li className="flex items-center space-x-3">
+                      <svg className="flex-shrink-0 w-5 h-5 text-cyan-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                      <span className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">Individual configuration</span>
+                  </li>
+                  <li className="flex items-center space-x-3">
+                      <svg className="flex-shrink-0 w-5 h-5 text-cyan-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                      <span className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">No setup, or hidden fees</span>
+                  </li>
+                  <li className="flex items-center space-x-3">
+                      <svg className="flex-shrink-0 w-5 h-5 text-cyan-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                      <span className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">Team size: <span className="font-semibold">10 developers</span></span>
+                  </li>
+                  <li className="flex items-center space-x-3">
+                      <svg className="flex-shrink-0 w-5 h-5 text-cyan-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                      <span className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">Premium support: <span className="font-semibold">24 months</span></span>
+                  </li>
+                  <li className="flex items-center space-x-3">
+                      <svg className="flex-shrink-0 w-5 h-5 text-cyan-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                      <span className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">Free updates: <span className="font-semibold">24 months</span></span>
+                  </li>
+              </ul>
+<Button link="https://google.com"/>          </div>
+          <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-white bg-black-gradient-2 rounded-lg border border-gray-100 shadow border-gray-600 xl:p-8 bg-gray-800 text-white">
+          <h3 class="mb-4 font-poppins font-semibold text-[33px] leading-[32px] text-white">Herman Jensen</h3>
+              <p className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] text-white sm:text-lg text-white">Best for large scale uses and extended redistribution rights.</p>
+              <div className="flex justify-center items-baseline my-8">
+                  <span className="mr-2 text-5xl font-extrabold">$499</span>
+                  <span className="text-white text-white">/month</span>
+              </div>
+              <ul role="list" className="mb-8 space-y-4 text-left">
+                  <li className="flex items-center space-x-3">
+                      <svg className="flex-shrink-0 w-5 h-5 text-cyan-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                      <span className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">Individual configuration</span>
+                  </li>
+                  <li className="flex items-center space-x-3">
+                      <svg className="flex-shrink-0 w-5 h-5 text-cyan-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                      <span className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">No setup, or hidden fees</span>
+                  </li>
+                  <li className="flex items-center space-x-3">
+                      <svg className="flex-shrink-0 w-5 h-5 text-cyan-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                      <span className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">Team size: <span className="font-semibold">100+ developers</span></span>
+                  </li>
+                  <li className="flex items-center space-x-3">
+                      <svg className="flex-shrink-0 w-5 h-5 text-cyan-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                      <span className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">Premium support: <span className="font-semibold">36 months</span></span>
+                  </li>
+                  <li className="flex items-center space-x-3">
+                      <svg className="flex-shrink-0 w-5 h-5 text-cyan-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                      <span className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">Free updates: <span className="font-semibold">36 months</span></span>
+                  </li>
+              </ul>
+<Button />          </div>
+      </div>
+  </div>
+</section>
 );
 
 export default CTA;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,6 @@
 import styles from "../style";
 import { discount, robot } from "../assets";
 import GetStarted from "./GetStarted";
-
 const Hero = () => {
   return (
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
@@ -9,8 +8,7 @@ const Hero = () => {
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
           <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
           <p className={`${styles.paragraph} ml-2`}>
-            <span className="text-white">20%</span> Discount For{" "}
-            <span className="text-white">1 Month</span> Account
+            <span className="text-white">1 Week</span> Free Trial <span className="text-white font-semibold">SPECIAL</span>
           </p>
         </div>
 
@@ -25,7 +23,7 @@ const Hero = () => {
         </div>
 
         <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
-          Payment Method.
+          Betting Service.
         </h1>
         <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
           Our team of experts uses a methodology to identify the credit cards
@@ -35,7 +33,7 @@ const Hero = () => {
       </div>
 
       <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" data-aos="zoom-in"/>
 
         {/* gradient start */}
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
