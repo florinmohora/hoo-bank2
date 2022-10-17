ad53db9130592823f9b7615e349b4a525cd4257f
Mihnea Georgescu <mihtitan@gmail.com>
2022-10-17 13:38:17 +0300

    asdasdasd

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,8 +7,8 @@ const Hero = () => {
       <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2" data-aos="zoom-in-down">
           <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
-          <p className={`${styles.paragraph} ml-2`}>
-            <span className="text-white">1 Week</span> Free Trial <span className="text-white font-semibold">SPECIAL</span>
+          <p className={`${styles.paragraph} ml-2 text-gradient font-bold`}>
+              1 Week Free Trial Special
           </p>
         </div>
 
