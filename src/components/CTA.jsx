5454e7474b968962acbb1586015af64727548416
florinmohora <97307746+florinmohora@users.noreply.github.com>
2022-10-18 20:50:28 +0300

    Pricing2

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -9,7 +9,7 @@ const CTA = () => (
          
           <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-white bg-black-gradient-2 rounded-lg   shadow  xl:p-8 ">
           <h3 class="mb-4 font-poppins font-semibold text-[33px] leading-[32px] text-white">3 Months</h3>
-              <p className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] text-white sm:text-lg">Relevant for multiple users, extended & premium support.</p>
+              <p className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] text-white sm:text-lg">For the members that want to take it to the next step</p>
               <div className="flex justify-center items-baseline my-8">
                   <span className="mr-2 text-5xl font-extrabold">$40</span>
                   <span className="text-white">/month</span>
@@ -18,11 +18,11 @@ const CTA = () => (
               <ul role="list" className="mb-8 space-y-4 text-left">
                   <li className="flex items-center space-x-3">
                       <svg className="flex-shrink-0 w-5 h-5 text-cyan-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                      <span className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">Individual configuration</span>
+                      <span className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">Unlimited predictions</span>
                   </li>
                   <li className="flex items-center space-x-3">
                       <svg className="flex-shrink-0 w-5 h-5 text-cyan-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                      <span className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">No setup, or hidden fees</span>
+                      <span className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">Personalized Bankroll Management</span>
                   </li>
                   <li className="flex items-center space-x-3">
                       <svg className="flex-shrink-0 w-5 h-5 text-cyan-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
@@ -48,11 +48,11 @@ const CTA = () => (
               <ul role="list" className="mb-8 space-y-4 text-left">
                   <li className="flex items-center space-x-3">
                       <svg className="flex-shrink-0 w-5 h-5 text-cyan-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                      <span className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">Individual configuration</span>
+                      <span className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">Unlimited predictions</span>
                   </li>
                   <li className="flex items-center space-x-3">
                       <svg className="flex-shrink-0 w-5 h-5 text-cyan-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                      <span className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">No setup, or hidden fees</span>
+                      <span className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">Basic Bankroll Management</span>
                   </li>
                   <li className="flex items-center space-x-3">
                       <svg className="flex-shrink-0 w-5 h-5 text-cyan-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
@@ -70,7 +70,7 @@ const CTA = () => (
 <Button link="https://google.com"/>          </div>
           <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-white bg-black-gradient-2 rounded-lg   shadow  xl:p-8  text-white">
           <h3 class="mb-4 font-poppins font-semibold text-[33px] leading-[32px] text-white">1 Month</h3>
-              <p className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] text-white sm:text-lg text-white">Best for large scale uses and extended redistribution rights.</p>
+              <p className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] text-white sm:text-lg text-white">Best for beginner or amateur betters.</p>
               <div className="flex justify-center items-baseline my-8">
                   <span className="mr-2 text-5xl font-extrabold">$50</span>
                   <span className="text-white">/month</span>
@@ -78,11 +78,11 @@ const CTA = () => (
               <ul role="list" className="mb-8 space-y-4 text-left">
                   <li className="flex items-center space-x-3">
                       <svg className="flex-shrink-0 w-5 h-5 text-cyan-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                      <span className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">Individual configuration</span>
+                      <span className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">Unlimited predictions</span>
                   </li>
                   <li className="flex items-center space-x-3">
                       <svg className="flex-shrink-0 w-5 h-5 text-cyan-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
-                      <span className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">No setup, or hidden fees</span>
+                      <span className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]">Basic Bankroll Management</span>
                   </li>
                   <li className="flex items-center space-x-3">
                       <svg className="flex-shrink-0 w-5 h-5 text-cyan-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
